a6e2395d09f4f42fa886d76f690e5f6589411897
Erick <47347282+erickdominguez@users.noreply.github.com>
2021-04-26 01:40:08 -0600

    Bootstrap Jalando

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import firebase from 'firebase'
-
+import  './assets/css/main.css'
 
 
 const firebaseConfig = {
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,28 @@
 <template>
-    <div class="login">
-        <!-- Esta es la parte de arriba del login, donde dice Login -->
-        <h1>Log in</h1>
-        <!-- Creo que por aqui debe estar la imagen del logo-->
-        <!-- Creo que por aqui debe finalizar la imagen del logo-->
+    <div class="container-fluid">
+        <div class="login">
+            <!-- Esta es la parte de arriba del login, donde dice Login -->
+            <h1>Log in</h1>
+            <!-- Creo que por aqui debe estar la imagen del logo-->
+            <!-- Creo que por aqui debe finalizar la imagen del logo-->
 
-        <!--Aqui empieza el formulario del login-->
-        <form @submit.prevent="Login">
-            <input type="text" placeholder="Email" v-model="email">
-            <input type="password" placeholder="Contraseña" v-model="password">
-            <input type="submit" value="Iniciar Sesión">
-            <p>¿No tienes una cuenta? <router-link to="/register">Registrate aquí</router-link></p>
-        </form>
-        <!--Aqui termina el formulario del login-->
-    </div>
-    <!--Aqui empieza el boton de login con gmail-->
-    <div class="logInGoogle">
-        <button @click="LoginG">Inicia sesion aquí con tu cuenta Google</button>
-    </div>
-    <!--Aqui termina el boton de login con gmail-->
-   
+            <!--Aqui empieza el formulario del login-->
+            <form @submit.prevent="Login">
+                <input type="text" placeholder="Email" v-model="email">
+                <input type="password" placeholder="Contraseña" v-model="password">
+                <input type="submit" value="Iniciar Sesión">
+                <p>¿No tienes una cuenta? <router-link to="/register">Registrate aquí</router-link></p>
+            </form>
+            <!--Aqui termina el formulario del login-->
+        </div>
+        <!--Aqui empieza el boton de login con gmail-->
+        <div class="logInGoogle">
+            <button @click="LoginG">Inicia sesion aquí con tu cuenta Google</button>
+        </div>
+        <!--Aqui termina el boton de login con gmail-->
+        </div>
+    <div>
+</div>
 </template>
 
 <script>
