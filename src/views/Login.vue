187b274a3da7c138312ebcddae7927df4cce16be
Erick <47347282+erickdominguez@users.noreply.github.com>
2021-04-27 00:57:00 -0600

    Pantalla de inicio terminada

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,31 @@
 <template>
     <div class="container-fluid gradient back">
         <div class="login">
-             <h1 class="pad-top">bienvenido de<br>nuevo, estudiante</h1>
+             <h1 class="pad-top title">bienvenido de<br>nuevo, estudiante</h1>
             <div class="pad-top">
                 <img src="../assets/logo.png" style="width:11%; min-width:140px"  >
             </div>
             
             <!-- Esta es la parte de arriba del login, donde dice Login -->
-            <h1 class="pad-top">REGISTRATE</h1>
+            <h1 class="pad-top">INICIA SESIÓN</h1>
 
             <!--Aqui empieza el formulario del login-->
-            <form @submit.prevent="Login">
+            <form @submit.prevent="Login" class="form-in">
                 <input type="text" class="inp" placeholder="email" v-model="email"><br>
                 <input type="password" class="inp" placeholder="contraseña" v-model="password"><br>
-                <input type="submit" class="btn-inis" value="Iniciar Sesión">
+                <div style="padding-top:10px"></div>
+                <input type="submit" class="btn yellow-button" value="aceptar">
             </form>
+            <div style="padding-top:20px"></div>
             <div class="logInGoogle">
-                <button @click="LoginG" class="btn-google">Inicia sesion aquí con tu cuenta Google</button>
+                <img class="btn-google" src="../assets/google.png" @click="LoginG" style="width:40%">
             </div>
-            <h1>O</h1>  
+            
+            <h1 id="o">o puedes </h1>  
+            <div id=bord></div>
+            <div id=bord1></div>
             <div class="btn yellow-button">
-                <router-link to="/register"><h1>crea una <br> cuenta nueva</h1></router-link>
+                <router-link to="/register">crear una cuenta nueva</router-link>
             </div>
                 
             <!--Aqui termina el formulario del login-->
