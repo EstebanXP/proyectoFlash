d2b29f6a209f2149316c0402cfc8ccef8e3bcc42
Efrain Estrada <a325060@uach.mx>
2021-04-27 12:14:23 -0600

    ya medio jala lo mio

--- src/views/Config-notis.vue
+++ src/views/Config-notis.vue
@@ -1,18 +1,17 @@
 <template>
-    <div class="container-fluid gradient back">
+    <div class="container-fluid">
         <!--Esta es la parte de arriba del cuaduito, donde va el logo y la flecha-->
-        <div class="pad-top">
-                <img src="../assets/logo.png" style="width:11%; min-width:140px"  >
+        <div class="pad-top-events">
+                <img src="../assets/logo.png" style="width:2%; min-width:80px">
         </div>
-
+        <div  id=backEvents>
         <!--Se supone que aqui esta el cuaduito pero no se como llamarlo para verlo-->
-        <div id="cuaduitoEventos">
-                <h1>Mostrar proximos eventos</h1>
-        <!--Perdon Erick maniana me ayudas a hacerlo que se vea, 
-        preferi borrar todo el cagadero que tenaia, solo deje segunyo la estructura-->
-        </div>
-    <div>
-</div>
+            <div>
+                <h1 id="h1Events">Mostrar proximos eventos</h1>
+        
+            </div>    
+        </div>   
+    </div>
 </template>
 
 
