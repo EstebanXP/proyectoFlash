923464e47c93bf5f2c552992ec07516f4d933536
Efrain Estrada <a325060@uach.mx>
2021-04-28 15:47:41 -0600

    Ya esta bonita solo falta funcionalidad

--- src/views/Config-notis.vue
+++ src/views/Config-notis.vue
@@ -1,41 +1,39 @@
 <template>
-    <div class="container-fluid">
-        <!--Esta es la parte de arriba del cuaduito, donde va el logo y la flecha-->
-        <div class="pad-top-events gradient">
-                <img src="../assets/logo.png" style="width:2%; min-width:80px">
-        </div>
-        <div  id=backEvents>
-                <h1 id="h1Events">Mostrar proximos eventos</h1>
-                <form @submit.prevent="conf" class="form-in">
-                <div style="padding-top:10px"></div>
-            
-                <div  class="botones">
-                    <p style="color: black; " id="sem">1 semana</p>
-    
-                    <input style="float:left;" type="submit" class=" dot" value="">
-                </div>
-                <br>
-                <div  class="botones">
-                    <p style="color: black; ">2 semanas</p>
-                    
-                    <input style="float:left;" type="submit" class=" dot-fill" value="">
-                </div>
-                <br>
-                <div  class="botones">
-                    <p style="color: black; ">1 mes</p>
-                    
-                    <input style="float:left;" type="submit" class=" dot-fill" value="">
-                </div>
-                <br>
-                <div  class="botones">
-                    <p style="color: black; ">todos</p>
-                    
-                    <input style="float:left;" type="submit" class=" dot-fill" value="">
-                </div>
-               
-            </form>
-        </div>   
+  <div class="container-fluid">
+    <!--Esta es la parte de arriba del cuaduito, donde va el logo y la flecha-->
+    <div class="pad-top-events gradient">
+      <img src="../assets/logo.png" style="width: 2%; min-width: 80px" />
     </div>
+    <div id="backEvents">
+      <h1 id="h1Events">Mostrar proximos eventos</h1>
+      <form @submit.prevent="conf" class="form-in">
+        <div style="padding-top: 10px"></div>
+
+        <div class="container" style="color: #FF3366">
+          <div class="row">
+            <div class="col">1 Semana</div>
+            <div class="col"><input style="float: right" type="submit" class="dot" value="" /></div>
+          </div>
+
+          <div class="row">
+            <div class="col">2 Semanas</div>
+            <div class="col"><input style="float: right" type="submit" class="dot-fill" value="" /></div>
+          </div>
+
+          <div class="row">
+            <div class="col">1 Mes</div>
+            <div class="col"><input style="float: right" type="submit" class="dot-fill" value="" /></div>
+          </div>
+
+          <div class="row">
+            <div class="col">Todos</div>
+            <div class="col"><input style="float: right" type="submit" class="dot-fill" value="" /></div>
+          </div>
+          
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 
